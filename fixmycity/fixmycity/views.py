b3af7a73bf7b8d0590bd50e394f65522d513ab6a
Siddharth Saxena <sidcoder73@gmail.com>
2026-08-09 20:28:24 +0530

    implement  templates folder and give dir in settings.py

--- fixmycity/fixmycity/views.py
+++ fixmycity/fixmycity/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import  render
 
 def home(request):
-    return HttpResponse("Hello World,You are siddharth saxena")
+    return render(request,"website/index.html")
 
 def about(request):
     return HttpResponse("This is my About page")
